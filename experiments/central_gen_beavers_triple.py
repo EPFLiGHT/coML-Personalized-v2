660147f767c34ac9f474f3a45d07827772068b70
LieHe <liam.he15@gmail.com>
2020-02-21 12:20:37 +0100

    Add some simple scripts for training nn on 1 worker, 1ServerNWorker, 2ServerNWorker.
    Note that the code needs to be optimized; the byzantine part has not been implemented. The 0,1,2 server case should give same results under `mpirun -np ...`

---
+++ experiments/central_gen_beavers_triple.py
@@ -0,0 +1,37 @@
+"""
+This script generates Beaver's triple in a centralized way using `c_i = a_i * sum_j b_j`
+
+For each party output a dictionary : {"a": ..., "b": ..., "c": ...}
+
+Notes:
+1. Decentralized generation: fig 7. of "Overdrive: making SPDZ great again".
+1. Use integer instead of float.
+"""
+import argparse
+import numpy as np
+import os
+import pickle
+
+parser = argparse.ArgumentParser(description='Generate Beavers triples.')
+parser.add_argument('-N', type=int, default=10,
+                    help='Number of Beavers triples to generate.')
+parser.add_argument('-NP', type=int, default=2,
+                    help='Number of parties.')
+parser.add_argument('-f', type=str, default='/datasets/bv',
+                    help='The directory of generated files: e.g. bv/1.pickle, bv/2.pickle')
+parser.add_argument('-seed', type=int, default=0,
+                    help='Seed.')
+args = parser.parse_args()
+
+np.random.seed(args.seed)
+
+# raise FileExistsError if exists
+os.mkdir(args.f)
+
+a = np.random.uniform(low=0.0, high=1.0, size=(args.N, args.NP))
+b = np.random.uniform(low=0.0, high=1.0, size=(args.N, args.NP))
+c = a * np.c_[b.sum(axis=1)]
+
+for i in range(args.NP):
+    with open(args.f + "/{}.pickle".format(i + 1), 'wb') as f:
+        pickle.dump({"a": a[:, i], "b": b[:, i], "c": c[:, i]}, f)
