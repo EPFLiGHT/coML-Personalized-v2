8b5b76cd09b8f8821c78951fa9886f42abc3ce1d
Felix Grimberg <felix.grimberg@epfl.ch>
2020-05-11 11:45:46 +0200

    Move custom types and utils to module. WARNING:
    Kernel keeps dying on iccluster033.

---
+++ module/custom_types.py
@@ -0,0 +1,61 @@
+from typing import Any, Generator, Tuple, Mapping, Sequence, Optional, Callable, Union, NamedTuple
+from collections import namedtuple
+
+import haiku as hk
+import jax.numpy as jnp
+import tensorflow as tf
+
+Batch = Mapping[str, jnp.ndarray]
+DatasetSequence = Sequence[tf.data.Dataset]
+LossFunction = Callable[[hk.Params, Batch], jnp.ndarray]
+OptState = Any
+ClientState = namedtuple("ClientState", "next_idx\
+                                         epoch_count\
+                                         similarity",
+                         defaults=[int(0), int(0), float(1)]
+                        )
+
+
+# define hyperparameters format.
+ServerHyperParams = namedtuple("ServerHyperParams", "num_rounds\
+                                                     max_batches_per_round\
+                                                     max_epochs_per_round\
+                                                     batch_size\
+                                                     seed")
+
+# Define the aggregator format
+SimilarityAggregatorHyperParams = namedtuple("SimilarityAggregatorHyperParams", "distance_penalty_factor")
+AverageAggregatorHyperParams = namedtuple("AverageAggregatorHyperParams", "")
+AggregatorHyperParams = Union[SimilarityAggregatorHyperParams, AverageAggregatorHyperParams]
+AggregatorFunction = Callable[[Sequence[hk.Params], Sequence[ClientState], AggregatorHyperParams], Tuple[hk.Params, Sequence[ClientState]]]
+class Aggregator(NamedTuple):
+    aggregator_function: AggregatorFunction
+    aggregator_hyperparams: AggregatorHyperParams
+
+# message to the client from server.
+ClientMessage = namedtuple("ClientMessage", "params\
+                                             opt_init_input")
+
+# message to the server from client.
+ServerMessage = namedtuple("ServerMessage", "aggregator_input\
+                                             stateupdater_input")
+
+
+# message to the server from client for book keeping.
+DiagnosticsMessage = namedtuple("DiagnosticsMessage", "train_loss\
+                                                       train_acc\
+                                                       test_loss\
+                                                       test_acc\
+                                                       weight")
+ClientOutput = Tuple[ServerMessage, DiagnosticsMessage]
+
+# State storing the diagnostics history
+dstate_fields = ["train_loss_global",
+                 "train_accuracy_global",
+                 "test_loss",
+                 "test_accuracy",
+                 "similarities",
+                 "epoch_counts"]
+DiagnosticsState = namedtuple("DiagnosticsState", dstate_fields,
+                              defaults= [list()]*len(dstate_fields)
+                             )
